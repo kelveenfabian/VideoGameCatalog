a6a6d48bd329b32ec92b444cb204746171446531
kelveenfabian <kelveenfabian@pursuit.org>
2019-03-31 00:05:26 -0400

    Created SplashActivity for splash screen

---
+++ app/src/main/java/com/example/videogamecatalog/SplashActivity.java
@@ -0,0 +1,19 @@
+package com.example.videogamecatalog;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.SystemClock;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        SystemClock.sleep(1500);
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
