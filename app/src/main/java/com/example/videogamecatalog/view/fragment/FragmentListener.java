bdfcb7edd1ff304e2f14c9c9ed05dda9003ce768
kelveenfabian <kelveenfabian@pursuit.org>
2019-04-08 21:56:51 -0400

    App allows the user to receive a list of genre's in a fragment that when clicked takes the user to another fragment that shows them a list of games that pertain to that genre

---
+++ app/src/main/java/com/example/videogamecatalog/view/fragment/FragmentListener.java
@@ -0,0 +1,10 @@
+package com.example.videogamecatalog.view.fragment;
+
+import com.example.videogamecatalog.model.Game;
+
+import java.util.List;
+
+public interface FragmentListener {
+
+    void GenreToGamesFrag(List<Game> gamesList);
+}
